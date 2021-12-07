0a595f94ab3235c89cb744344ffb15f150bdecf3
Youhanna-Yousry <youhanna.yousry.2580@gmail.com>
2021-12-07 21:04:57 +0200

    adding empty line component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SketchComponent } from './components/sketch/sketch.component';
 import { CircleComponent } from './components/circle/circle.component';
+import { LineComponent } from './components/line/line.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SketchComponent,
     CircleComponent,
+    LineComponent,
   ],
   imports: [
     BrowserModule
