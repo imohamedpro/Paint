493135da2bd91e330eec6de86254b9be4e7fe88d
Mohamed Magdy <imohamedpro@icloud.com>
2021-12-08 00:44:14 +0200

    Removed old square component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { SketchComponent } from './components/sketch/sketch.component';
 import { CircleComponent } from './components/circle/circle.component';
