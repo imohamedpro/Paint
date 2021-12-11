8efb0dfe27db5e13aa63f0963088575aac4044bf
Karim Alaa <karimalaa1912@gmail.com>
2021-12-11 17:21:52 +0200

    userDefined clean

--- src/app/components/user-defined/user-defined.component.ts
+++ src/app/components/user-defined/user-defined.component.ts
@@ -1,7 +1,6 @@
-import { Component, HostListener, Input, OnInit } from '@angular/core';
-import { Point } from '../../classes/Point';
+import { Component, Input, OnInit } from '@angular/core';
+import { EventHandler } from '../../classes/EventHandler';
 import { Shape } from '../../classes/Shape';
-import { UserDefined } from '../../classes/UserDefined';
 import { ShapeManagerService } from '../../services/ShapeManager/shape-manager.service';
 
 @Component({
@@ -11,89 +10,22 @@ import { ShapeManagerService } from '../../services/ShapeManager/shape-manager.s
 })
 export class UserDefinedComponent implements OnInit {
   @Input('custom') shape!: any;
-  ready: boolean;
-  manager: ShapeManagerService;
-  initialClick!: Point;
-  dragging: boolean; 
-  ctrl: boolean;
+  eventHandler: EventHandler;
+
   constructor(manager: ShapeManagerService) { 
-    this.manager = manager;
-    this.dragging = false;
-    this.ctrl = false;
-    this.ready = false;
+    this.eventHandler = new EventHandler(manager);
   }
 
   ngOnInit(): void {
-    console.log("init");
-
     console.log(this.shape);
-    // console.log(this.shape.initialDims);
-    this.ready = true;
   }
   getScaleX(): number{
-    // console.log(this.ready);
-    if(this.ready){
-      return this.shape.dimensions[0] / this.shape.initialDims[0];
-    }
-    return 1;
+    return this.shape.dimensions[0] / this.shape.initialDims[0];
   }
   getScaleY(): number{
-    if(this.ready){
-      return this.shape.dimensions[1] / this.shape.initialDims[1];
-    }
-    return 1;
+    return this.shape.dimensions[1] / this.shape.initialDims[1];
   }
   pointsGetter(shape: Shape): string {
     return `${shape.center.x},${shape.center.y} ${shape.dimensions[0]},${shape.dimensions[1]} ${shape.dimensions[2]},${shape.dimensions[3]}`;
-  }
-
-
-
-  
-  @HostListener('window:keydown', ['$event'])
-  ctrlDown(event: KeyboardEvent){
-    console.log(event);
-    if(event.key == 'Control'){
-      console.log('ctrl down');
-      this.ctrl = true;
-    }
-    else if(event.key === 'Delete'){
-      this.manager.delete();
-      console.log('delete is down');
-    }
-  }
-  @HostListener('window:keyup', ['$event'])
-  ctrlUp(event: KeyboardEvent){
-    if(event.key == 'Control'){
-      console.log("ctrl up");
-      this.ctrl = false;
-    }
-  }
-
-
-  clicked(): void {
-    if(!this.dragging){
-      if(this.manager.selectedShapes.size == 0){  //first one to be clicked
-        this.manager.select(this.shape);
-
-      }else if (this.shape.isSelected){
-        this.manager.deselect(this.shape);
-
-      }else if(!this.shape.isSelected && this.ctrl) {
-        this.manager.select(this.shape);
-      }else{
-        this.manager.clearSelected();
-        this.manager.select(this.shape);
-      }
-    }
-    console.log(this.manager);
-
-  }
-  mouseDown(e: MouseEvent):void {
-    if(e.button == 0 && this.shape.isSelected){
-      this.initialClick = new Point(e.clientX, e.clientY);
-      this.manager.setDragging(this.initialClick);
-      console.log(this.initialClick);
-    }
   }
 }
