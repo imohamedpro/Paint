699e9d93fbdff0e9ffa90d0755dbbce49696be1b
Karim Alaa <karimalaa1912@gmail.com>
2021-12-11 17:17:00 +0200

    square clean

--- src/app/components/square/square.component.ts
+++ src/app/components/square/square.component.ts
@@ -1,9 +1,7 @@
 import { Shape } from '../../classes/Shape';
-import { Component, HostListener, Input, OnInit } from '@angular/core';
-import { Point } from '../../classes/Point';
-import { Color, FillColor, StrokeColor } from '../../classes/Style';
-import { IShape } from '../../interfaces/IShape'
+import { Component, Input, OnInit } from '@angular/core';
 import { ShapeManagerService } from '../../services/ShapeManager/shape-manager.service';
+import { EventHandler } from '../../classes/EventHandler';
 
 @Component({
   selector: '[shape=square]',
@@ -11,105 +9,11 @@ import { ShapeManagerService } from '../../services/ShapeManager/shape-manager.s
   styleUrls: ['./square.component.css']
 })
 export class SquareComponent implements OnInit {
-  // This decorator @Input() exposes our property (circle) to the world .. any parent component can use it.
-  // Parent component: any component hosting this one through its selector.
   @Input() square!: Shape;
-  manager: ShapeManagerService;
-  initialClick!: Point;
-  dragging: boolean = false; 
-  ctrl: boolean;
+  eventHandler: EventHandler;
 
   constructor(manager: ShapeManagerService) { 
-    this.manager = manager;
-    this.dragging = false;
-    this.ctrl = false;
+    this.eventHandler = new EventHandler(manager);
   }
-  ngOnInit(): void {
-    
-  }
-
-  // clicked(): void {
-  //   if(!this.dragging){
-  //     this.square.isSelected = !this.square.isSelected;
-  //     if(this.square.isSelected){
-  //       this.manager.select(this.square);
-  //     }else{
-  //       // this.manager.deselect(this.square.id);
-  //     }
-  //   }
-  //   this.square.setFill(new FillColor(14, 145, 14, 0.5));
-  //   this.square.setStroke(new StrokeColor(0,0,0, 0.5));
-  // }
-
-  // mouseDown(e: MouseEvent): void {
-  //   if(e.button == 0){    // left click only
-  //     this.dragging = true;
-  //     this.initialClick = new Point(e.clientX, e.clientY);
-  //     console.log(this.initialClick);
-  //   }
-  // }
-  // move(e: MouseEvent): void {
-  //   if(e.button == 0 && this.square.isSelected && this.dragging){
-  //     let offset: Point = new Point(e.clientX - this.initialClick.x, e.clientY - this.initialClick.y);
-  //     console.log(offset);
-  //     this.initialClick = new Point(e.clientX, e.clientY);
-  //     this.manager.move(offset);
-  //   }
-  // }
-  // mouseUp(e: MouseEvent): void {
-  //   if(this.dragging){
-  //     this.dragging = false;
-  //   }
-  // }
-
-  @HostListener('window:keydown', ['$event'])
-  ctrlDown(event: KeyboardEvent){
-    console.log(event);
-    if(event.key == 'Control'){
-      console.log('ctrl down');
-      this.ctrl = true;
-    }
-  }
-  @HostListener('window:keyup', ['$event'])
-  ctrlUp(event: KeyboardEvent){
-    if(event.key == 'Control'){
-      console.log("ctrl up");
-      this.ctrl = false;
-    }
-  }
-
-  clicked(): void {
-    if(!this.dragging){
-      if(this.manager.selectedShapes.size == 0){  //first one to be clicked
-        this.manager.select(this.square);
-
-      }else if (this.square.isSelected){
-        this.manager.deselect(this.square);
-
-      }else if(!this.square.isSelected && this.ctrl) {
-        this.manager.select(this.square);
-      }else{
-        this.manager.clearSelected();
-        this.manager.select(this.square);
-      }
-
-      // if(this.square.isSelected){
-      //   this.square.setFill(new Color(Math.floor(Math.random()*255),Math.floor(Math.random()*255),Math.floor(Math.random()*255), 1));
-      //   this.square.setStroke(new Color(Math.floor(Math.random()*255),Math.floor(Math.random()*255),Math.floor(Math.random()*255), 1));
-      //   console.log(this.manager);
-      // }
-    }
-
-  }
-  mouseDown(e: MouseEvent):void {
-    if(e.button == 0 && this.square.isSelected){
-      this.initialClick = new Point(e.clientX, e.clientY);
-      this.manager.setDragging(this.initialClick);
-      console.log(this.initialClick);
-    }
-  }
-
-  // getSideLength():number{
-  //   return Math.max(this.square.dimensions[0],this.square.dimensions[1]);
-  // }
+  ngOnInit(): void {}
 }
